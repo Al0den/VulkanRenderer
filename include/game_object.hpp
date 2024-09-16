a171249571141a16eb260951eb121d0993ef6f3b
Al0den <111601320+Al0den@users.noreply.github.com>
2024-09-16 16:12:46 +0200

    3d cube

--- include/game_object.hpp
+++ include/game_object.hpp
@@ -2,22 +2,30 @@
 
 #include "model.hpp"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <memory>
 
 namespace vkengine {
 
-struct Transform2dComponent {
-    glm::vec2 translation{};
-    glm::vec2 scale{1.f, 1.f};
+struct TransformComponent {
+    glm::vec3 translation{};
+    glm::vec3 scale{1.f, 1.f, 1.f};
 
-    float rotation;
+    glm::vec3 rotation{};
 
-    glm::mat2 mat2() { 
-        const float s = sin(rotation);
-        const float c = cos(rotation);
-        glm::mat2 rotMatrix{{c, s}, {-s, c}};
-        glm::mat2 scaleMat{{scale.x, 0.f}, {0.f, scale.y}};
-        return rotMatrix * scaleMat; 
+
+    // Matrices: Translation * Ry * Rx * Rz * scaleA
+    // Tait bryan angles
+    glm::mat4 mat4() {
+        auto transform = glm::translate(glm::mat4{1.f}, translation);
+
+        transform = glm::rotate(transform, rotation.y, {0.f, 1.f, 0.f});
+        transform = glm::rotate(transform, rotation.x, {1.f, 0.f, 0.f});
+        transform = glm::rotate(transform, rotation.z, {0.f, 0.f, 1.f});
+
+        transform = glm::scale(transform, scale);
+        return transform;
     }
 };
 
@@ -40,7 +48,7 @@ class GameObject {
         std::shared_ptr<Model> model{};
         glm::vec3 color{};
 
-        Transform2dComponent transform2d;
+        TransformComponent transform{};
 
     private:
         GameObject(id_t objId) : id(objId) {}
--- src/simple_render_system.cpp
+++ src/simple_render_system.cpp
@@ -11,8 +11,7 @@
 using namespace vkengine;
 
 struct SimplePushConstantData {
-    glm::mat2 transform{1.f};
-    glm::vec2 offset;
+    glm::mat4 transform{1.f};
     alignas(16) glm::vec3 color;
 };
 
@@ -57,10 +56,13 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::v
     pipeline->bind(commandBuffer);
 
     for (auto &obj : gameObjects) {
+        obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
+        obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.005f, glm::two_pi<float>());
+
         SimplePushConstantData push{};
-        push.transform = obj.transform2d.mat2();
-        push.offset = obj.transform2d.translation;
+        push.transform = obj.transform.mat4();
         push.color = obj.color;
+
         vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
         obj.model->bind(commandBuffer);
         obj.model->draw(commandBuffer);
